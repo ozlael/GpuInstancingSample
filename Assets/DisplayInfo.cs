c6c9569f0c3b6384e103e4634d9c0c594155915e
ozlael <ozjjangozjjang@gmail.com>
2018-01-03 05:24:04 +0900

    Initial commit

---
+++ Assets/DisplayInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayInfo : MonoBehaviour {
+
+	[SerializeField]
+	Text textFPS;
+	[SerializeField]
+	Text textDevice;
+
+	float frames = 0f;
+	float timeElap = 0f;
+
+	void Start() {
+		textDevice.text = "" + SystemInfo.graphicsDeviceName + ", " + SystemInfo.graphicsDeviceVersion;
+	}
+
+	void Update() {
+		frames++;
+		timeElap += Time.unscaledDeltaTime;
+		if (timeElap > 1f) {
+			timeElap -= 1f;
+			UpdateText ();
+			frames =0;
+		}
+	}
+
+	void UpdateText() {
+		textFPS.text = string.Format (
+			"FPS : {0}, FrameTime : {1:F2} ms",
+			frames, Time.unscaledDeltaTime * 1000.0f);
+	}
+}
---
+++ Assets/ObjectCloneGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCloneGenerator : MonoBehaviour {
+
+	public int cnt;
+	public float gap;
+	public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < cnt; i++) {
+			for (int j = 0; j < cnt; j++) {
+				for (int k = 0; k < cnt; k++) { 
+					Instantiate (obj, new Vector3 (i * gap, -j * gap, k * gap), Quaternion.identity);
+				}
+			}
+		}
+	}
+}
